4a4b8a7535859575b12a83f67077ac505517578e
FreshDev <billyhans90@gmail.com>
2022-06-08 00:31:02 +0100

    Add the functions for api requests

---
+++ lib/constant.dart
@@ -0,0 +1,13 @@
+// ------STRINGS-----
+const baseURL = 'http://127.0.0.1:8000/api';
+const loginURL = '$baseURL/login';
+const registerURL = '$baseURL/register';
+const logoutURL = '$baseURL/logout';
+const userURL = '$baseURL/user';
+const productsURL = '$baseURL/products';
+const commentsURL = '$baseURL/comments';
+
+// -----ERRORS-----
+const serverError = 'Server error';
+const unauthorized = 'Unauthorized';
+const somethingWentWrong = 'Something went wrong, try again';
